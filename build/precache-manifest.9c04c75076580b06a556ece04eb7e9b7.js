132f454a21fecf511bdc405cd65a8f58563fe7f9
Eric Hallow <62398833+erhallow@users.noreply.github.com>
2020-07-31 17:57:21 -0500

    update build

---
+++ build/precache-manifest.9c04c75076580b06a556ece04eb7e9b7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aade93b31d989fbd6136318c926733d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8630a8c1c43a0a6fe13b",
+    "url": "/static/css/main.ccaae508.chunk.css"
+  },
+  {
+    "revision": "ea0c0c7cb9b4d6b9976f",
+    "url": "/static/js/2.2418fcf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2418fcf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8630a8c1c43a0a6fe13b",
+    "url": "/static/js/main.36a5c133.chunk.js"
+  },
+  {
+    "revision": "fd7abb1def4ace2f9f8c",
+    "url": "/static/js/runtime-main.e89298f0.js"
+  },
+  {
+    "revision": "3af076c3b38cb0fe90440c22902bbb37",
+    "url": "/static/media/article1.3af076c3.png"
+  },
+  {
+    "revision": "636b82aa90bc21a7f184736ce36031fd",
+    "url": "/static/media/article2.636b82aa.png"
+  },
+  {
+    "revision": "9f943ed372c26247bb96c765da1b31e9",
+    "url": "/static/media/bears.9f943ed3.jpg"
+  },
+  {
+    "revision": "e5f88aaf8d03866f8e0375218f711387",
+    "url": "/static/media/chicago.e5f88aaf.jpg"
+  },
+  {
+    "revision": "5e05bd06d4504da5bd8ab79ac9af57d9",
+    "url": "/static/media/frisbee.5e05bd06.jpg"
+  },
+  {
+    "revision": "6a1407bd44ff48c985b0d80cf32ace03",
+    "url": "/static/media/intro.6a1407bd.jpg"
+  },
+  {
+    "revision": "0cf706de207b88297c935c523da1d286",
+    "url": "/static/media/poker.0cf706de.jpg"
+  },
+  {
+    "revision": "b3a3f4b425bb8542d3c0d96909412fd8",
+    "url": "/static/media/sudoku.b3a3f4b4.png"
+  },
+  {
+    "revision": "a65c2bc9b4d162288d906221bad70bcc",
+    "url": "/static/media/top10crypto.a65c2bc9.png"
+  },
+  {
+    "revision": "b656a361f787c84d67743fa7f96ddba0",
+    "url": "/static/media/walk.b656a361.jpg"
+  }
+]);
